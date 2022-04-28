f965d095e96f62256aa4a6bf88ade2b2ccaab429
IliaAzhel <aje-ilia2013@yandex.by>
2022-04-28 19:15:23 +0300

    update vars

--- AlgebraFarming/src/utils/constants.ts
+++ AlgebraFarming/src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { Address } from '@graphprotocol/graph-ts'
 
 
-export const FarmingCenterAddress = Address.fromString('0x5b1803B6a3Ee361dF563BB0FF11B92Ce3F16Ac4e')
-export const IncentiveFarmingAddress = Address.fromString('0xb7875b8Dc8f49c2D6BBb658905dBF88843B54Da8')
+export const FarmingCenterAddress = Address.fromString('0x11ba1642143d77ae030a38c6f07a7762b4e439b0')
+export const IncentiveFarmingAddress = Address.fromString('0x5af2b584f2f48bdd71d0564fc3bf978a9199ea3c')
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
