927fd6faf0a2c0995bfa955a40fb342a258e6c30
sameepsi <sameepsi@gmail.com>
2022-10-11 16:56:40 +0530

    commit

--- AlgebraFarming/src/utils/constants.ts
+++ AlgebraFarming/src/utils/constants.ts
@@ -1,3 +1,3 @@
 import { Address } from '@graphprotocol/graph-ts';
 
-export const FarmingCenterAddress = Address.fromString("0x3A1017b5C8e5bb5B768A7f39548C0D528a9bE115")
+export const FarmingCenterAddress = Address.fromString("0x82831E9565cb574375596eFc090da465283E22A4")
